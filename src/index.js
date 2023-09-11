28c33b79c5051309e0dc684247c915f70f402f01
Mmishaaa <kkochin2528@gmail.com>
2023-09-11 17:55:52 +0300

    init: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let nstr = n.toString();
+  let numbers = nstr.split('');
+  let result = '';
+  for (let i = numbers.length - 1; i >= 0; i--)
+  {
+    result += numbers[i];
+  }
+  return parseInt(result);
 }
+
+
